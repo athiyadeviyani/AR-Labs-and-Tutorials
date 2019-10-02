d086da8e54dce864f5539a01d1c7f8123a6f1089
Athiya Deviyani <athiyadee@gmail.com>
2019-10-02 19:01:25 +0100

    Tutorial 1 Isabelle Exercises

---
+++ tutorial_1.thy
@@ -0,0 +1,45 @@
+theory tutorial_1
+imports Main
+begin
+
+lemma 1: "(P \<longrightarrow> (Q \<longrightarrow> R)) \<longrightarrow> ((P \<longrightarrow> Q) \<longrightarrow> (P \<longrightarrow> R))"
+  apply (rule impI)+
+  apply (erule impE)
+   apply assumption
+  apply (erule mp)
+  apply (erule impE)
+   apply assumption
+  apply assumption
+
+lemma 2: "\<not>\<not>P \<longrightarrow> P"
+  apply (rule impI)+
+  apply (rule classical)
+  apply (erule notE)
+  apply assumption
+
+lemma 3: "(P \<longrightarrow> Q \<and> R) \<longrightarrow> ((P \<longrightarrow> Q) \<and> (P \<longrightarrow> R))"
+  apply (rule impI)
+  apply (rule conjI)
+   apply (rule impI)
+   apply (erule impE)
+  apply assumption
+   apply (erule conjE)
+   apply assumption
+  apply (rule impI)
+  apply (erule impE)
+   apply assumption
+  apply (erule conjE)
+  apply assumption
+
+lemma 4: "(\<not>P \<longrightarrow> Q) \<longrightarrow> (\<not>Q \<longrightarrow> P)"
+  apply (rule impI)+
+  apply (rule classical)
+  apply (erule notE)
+  apply (erule impE)
+   apply assumption
+  apply assumption
+
+lemma 5: "P \<or> \<not>P"
+  oops
+
+
