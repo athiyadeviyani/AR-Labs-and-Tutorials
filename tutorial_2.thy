4b66345869372d8cb5c320a9f7b7513811b644e9
Athiya Deviyani <athiyadee@gmail.com>
2019-10-15 12:27:12 +0100

    Finished lemma 3.

--- tutorial_2.thy
+++ tutorial_2.thy
@@ -17,10 +17,13 @@ lemma 2: "\<not>(\<exists> x. P x) \<Longrightarrow> \<forall> x. \<not>P x"
   apply assumption
 
 lemma 3: "\<not>(\<forall> x. P x) \<Longrightarrow> \<exists> x. \<not>P x"
-  apply (rule exI)
-  apply (rule notI)
+  apply (rule ccontr)
   apply (erule notE)
   apply (rule allI)
+  apply (rule ccontr)
+  apply (erule notE)
+  apply (rule exI)
+  by assumption
   
 
 end
