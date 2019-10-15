5d7be6d2c3b0d35ec951e285275b5acf36fd3bd3
Athiya Deviyani <athiyadee@gmail.com>
2019-10-15 13:53:02 +0100

    Update tutorial_3.thy

--- tutorial_3.thy
+++ tutorial_3.thy
@@ -5,23 +5,24 @@ Main
 begin
 
 locale group =
-  fixes on :: "'p \<Rightarrow> 'l \<Rightarrow> bool" 
-  assumes line_on_two_pts: "a \<noteq> b \<Longrightarrow> \<exists>l. on a l \<and> on b l"
-   and line_on_two_pts_unique: "\<lbrakk> a \<noteq> b; on a l; on b l; on a m; on b m \<rbrakk> \<Longrightarrow> l = m"
-  and two_points_on_line: "\<exists>a b. a \<noteq> b \<and> on a l \<and> on b l"
-  and three_points_not_on_line: "\<exists>a b c. a \<noteq> b \<and> a \<noteq> c \<and> b \<noteq> c \<and> 
-                                    \<not> (\<exists>l. on a l \<and> on b l \<and> on c l)"
+  fixes on :: "'p ⇒ 'l ⇒ bool" 
+  assumes line_on_two_pts: "a ≠ b ⟹ ∃l. on a l ∧ on b l"
+   and line_on_two_pts_unique: "⟦ a ≠ b; on a l; on b l; on a m; on b m ⟧ ⟹ l = m"
+  and two_points_on_line: "∃a b. a ≠ b ∧ on a l ∧ on b l"
+  and three_points_not_on_line: "∃a b c. a ≠ b ∧ a ≠ c ∧ b ≠ c ∧ 
+                                    ¬ (∃l. on a l ∧ on b l ∧ on c l)" 
 begin
 
 
-lemma exists_pt_not_on_line: "\<exists>x. \<not> on x l"
+lemma exists_a_point_not_on_line: "∃x. ¬ on x l"
 proof - 
-  obtain a b c where l3: "\<not> (on a l \<and> on b l \<and> on c l)" using three_points_not_on_line by blast
-  thus ?thesis by blast
+  obtain a b c where lines: "¬ (on a l ∧ on b l ∧ on c l)" 
+    using three_points_not_on_line by blast
+  then show ?thesis by blast
 qed
 
 
-lemma two_lines_through_each_point: "\<exists>l m. on x l \<and> on x m \<and> l \<noteq> m"
+lemma two_lines_through_each_point: "∃l m. on x l ∧ on x m ∧ l ≠ m"
   oops
 
 lemma two_lines_unique_intersect_pt: 
